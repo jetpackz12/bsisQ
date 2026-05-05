fe238ee590263f5e7358206595f3a669b2b12951
Joseph Jett Abela <jetpackzabela12@gmail.com>
2026-05-05 09:37:41 +0800

    feat/FinalExaminationIM

--- src/App.vue
+++ src/App.vue
@@ -8,31 +8,20 @@
       <div
         class="border border-white rounded-sm w-75 md:w-125 h-50 bg-white/50 flex flex-col justify-center items-center gap-2"
       >
-        <h1 class="text-white md:text-lg">Information Management</h1>
-        <template v-if="!isButtonDisable">
-          <button
-            class="border border-white w-3/5 py-4 text-white rounded-sm hover:bg-white hover:text-black"
-            :disabled="isButtonDisable"
-            @click="toggleExam"
-          >
-            Start Exam
-          </button>
-        </template>
-        <template v-else>
-          <input
-            class="border border-white w-3/5 p-2 rounded-sm text-black focus:outline-0"
-            type="password"
-            placeholder="Enter code: "
-            v-model.lazy="code"
-            @keyup.enter="toggleExam"
-          />
-          <button
-            class="border border-white w-3/5 py-4 text-white rounded-sm hover:bg-white hover:text-black"
-            @click="toggleExam"
-          >
-            Start Exam
-          </button>
-        </template>
+        <h1 class="text-white md:text-lg">Final Examination</h1>
+        <input
+          class="border border-white w-3/5 p-2 rounded-sm text-black focus:outline-0"
+          type="password"
+          placeholder="Enter code: "
+          v-model.lazy="code"
+          @keyup.enter="toggleExam"
+        />
+        <button
+          class="border border-white w-3/5 py-4 text-white rounded-sm hover:bg-white hover:text-black"
+          @click="toggleExam"
+        >
+          Start Exam
+        </button>
       </div>
     </div>
     <div
@@ -65,24 +54,19 @@
 export default {
   data() {
     return {
-      pdfUrl: "/file/IM-Sample.pdf", //IM-Midterm.pdf IM-Sample.pdf
+      pdfUrl: "/file/IM-Sample.pdf", //IM-Final-WAK.pdf IM-Final.pdf IM-Sample.pdf
       isShowExam: false,
       timeLeft: 0,
       timer: null,
-      isButtonDisable: false,
       info: "Click “Start” to begin the exam!",
       code: "",
     };
   },
   mounted() {
-    const examInfo = localStorage.getItem("examInfo");
     const examDone = localStorage.getItem("examDone");
     const endTime = localStorage.getItem("examEndTime");
 
-    if (examDone === "true") {
-      this.isButtonDisable = true;
-      this.info = examInfo;
-    } else if (endTime) {
+    if (endTime && examDone === "false") {
       const remaining = Math.floor((endTime - Date.now()) / 1000);
 
       if (remaining > 0) {
@@ -96,12 +80,12 @@ export default {
   },
   methods: {
     toggleExam() {
-      if (this.isButtonDisable && this.code.toLowerCase() === "") {
+      if (this.code.toLowerCase() === "") {
         this.info = "No code provided, Please enter code!";
         return;
       }
 
-      if (this.isButtonDisable && this.code.toLowerCase() !== "cc104zx") {
+      if (this.code.toLowerCase() !== "123qwe") {
         this.info = "Code is invalid";
         return;
       }
@@ -110,7 +94,6 @@ export default {
       this.code = "";
 
       const endTime = Date.now() + 3600 * 1000; // 1 hour 3600
-      localStorage.setItem("examInfo", "Click “Start” to begin the exam!");
       localStorage.setItem("examEndTime", endTime);
       localStorage.setItem("examDone", "false");
 
@@ -133,14 +116,6 @@ export default {
       this.timeLeft = 0;
       clearInterval(this.timer);
       this.isShowExam = false;
-      this.isButtonDisable = true;
-
-      localStorage.setItem(
-        "examInfo",
-        "Please enter the code and click “Start Exam.”",
-      );
-
-      this.info = "Please enter the code and click “Start Exam.”";
       localStorage.setItem("examDone", "true");
     },
   },
