1547e9263a0d2181278cd2508c1875c17fd0a3da
Joseph Jett Abela <jetpackzabela12@gmail.com>
2026-05-06 17:05:23 +0800

    refactor/ExaminationFile

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,7 @@
 export default {
   data() {
     return {
-      pdfUrl: "/file/IM-Final.pdf", //IM-Final-WAK.pdf IM-Final.pdf IM-Sample.pdf
+      pdfUrl: "/file/IM-Sample.pdf", //IM-Final-WAK.pdf IM-Final.pdf IM-Sample.pdf
       isShowExam: false,
       timeLeft: 0,
       timer: null,
@@ -85,7 +85,7 @@ export default {
         return;
       }
 
-      if (this.code.toLowerCase() !== "123qwe") {
+      if (this.code.toLowerCase() !== "123456x") {
         this.info = "Code is invalid";
         return;
       }
